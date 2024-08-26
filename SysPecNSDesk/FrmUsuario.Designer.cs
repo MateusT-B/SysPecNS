bf535063007622b6432c6d31a228f255fbdc7fa9
MATEUS TEIXEIRA BATISTA <mateus.tbatista@senacsp.edu.br>
2024-08-26 11:57:04 -0300

    teste

---
+++ SysPecNSDesk/FrmUsuario.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace SysPecNSDesk
+{
+    partial class FrmUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvUsuarios = new DataGridView();
+            clnid = new DataGridViewTextBoxColumn();
+            clnNome = new DataGridViewTextBoxColumn();
+            clmEmail = new DataGridViewTextBoxColumn();
+            clnNivel = new DataGridViewTextBoxColumn();
+            clnAtivo = new DataGridViewCheckBoxColumn();
+            btnInserir = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            txtId = new TextBox();
+            txtNome = new TextBox();
+            txtEmail = new TextBox();
+            txtSenha = new TextBox();
+            cmbNivel = new ComboBox();
+            chkAtivo = new CheckBox();
+            txtConfSenha = new TextBox();
+            label6 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dgvUsuarios).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvUsuarios
+            // 
+            dgvUsuarios.AllowUserToAddRows = false;
+            dgvUsuarios.AllowUserToDeleteRows = false;
+            dgvUsuarios.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvUsuarios.Columns.AddRange(new DataGridViewColumn[] { clnid, clnNome, clmEmail, clnNivel, clnAtivo });
+            dgvUsuarios.Location = new Point(164, 247);
+            dgvUsuarios.Name = "dgvUsuarios";
+            dgvUsuarios.ReadOnly = true;
+            dgvUsuarios.RowHeadersVisible = false;
+            dgvUsuarios.Size = new Size(500, 150);
+            dgvUsuarios.TabIndex = 0;
+            dgvUsuarios.CellContentClick += dgvUsuarios_CellContentClick;
+            // 
+            // clnid
+            // 
+            clnid.Frozen = true;
+            clnid.HeaderText = "ID";
+            clnid.Name = "clnid";
+            clnid.ReadOnly = true;
+            clnid.Width = 50;
+            // 
+            // clnNome
+            // 
+            clnNome.Frozen = true;
+            clnNome.HeaderText = "Nome";
+            clnNome.Name = "clnNome";
+            clnNome.ReadOnly = true;
+            clnNome.Width = 150;
+            // 
+            // clmEmail
+            // 
+            clmEmail.Frozen = true;
+            clmEmail.HeaderText = "Email";
+            clmEmail.Name = "clmEmail";
+            clmEmail.ReadOnly = true;
+            clmEmail.Width = 150;
+            // 
+            // clnNivel
+            // 
+            clnNivel.Frozen = true;
+            clnNivel.HeaderText = "Nível";
+            clnNivel.Name = "clnNivel";
+            clnNivel.ReadOnly = true;
+            // 
+            // clnAtivo
+            // 
+            clnAtivo.Frozen = true;
+            clnAtivo.HeaderText = "Ativo";
+            clnAtivo.Name = "clnAtivo";
+            clnAtivo.ReadOnly = true;
+            clnAtivo.Width = 50;
+            // 
+            // btnInserir
+            // 
+            btnInserir.Location = new Point(250, 212);
+            btnInserir.Name = "btnInserir";
+            btnInserir.Size = new Size(75, 23);
+            btnInserir.TabIndex = 1;
+            btnInserir.Text = "&Inserir";
+            btnInserir.UseVisualStyleBackColor = true;
+            btnInserir.Click += btnInserir_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(250, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(18, 15);
+            label1.TabIndex = 2;
+            label1.Text = "ID";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(379, 21);
+            label2.Name = "label2";
+            label2.Size = new Size(40, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Nome";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(250, 89);
+            label3.Name = "label3";
+            label3.Size = new Size(36, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Email";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(529, 89);
+            label4.Name = "label4";
+            label4.Size = new Size(34, 15);
+            label4.TabIndex = 5;
+            label4.Text = "Nível";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(250, 151);
+            label5.Name = "label5";
+            label5.Size = new Size(39, 15);
+            label5.TabIndex = 6;
+            label5.Text = "Senha";
+            // 
+            // txtId
+            // 
+            txtId.Location = new Point(250, 39);
+            txtId.Name = "txtId";
+            txtId.Size = new Size(59, 23);
+            txtId.TabIndex = 7;
+            // 
+            // txtNome
+            // 
+            txtNome.Location = new Point(348, 39);
+            txtNome.Name = "txtNome";
+            txtNome.Size = new Size(262, 23);
+            txtNome.TabIndex = 8;
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(250, 107);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(219, 23);
+            txtEmail.TabIndex = 9;
+            // 
+            // txtSenha
+            // 
+            txtSenha.Location = new Point(250, 169);
+            txtSenha.Name = "txtSenha";
+            txtSenha.Size = new Size(100, 23);
+            txtSenha.TabIndex = 10;
+            txtSenha.UseSystemPasswordChar = true;
+            // 
+            // cmbNivel
+            // 
+            cmbNivel.FormattingEnabled = true;
+            cmbNivel.Location = new Point(489, 107);
+            cmbNivel.Name = "cmbNivel";
+            cmbNivel.Size = new Size(121, 23);
+            cmbNivel.TabIndex = 11;
+            // 
+            // chkAtivo
+            // 
+            chkAtivo.AutoSize = true;
+            chkAtivo.Location = new Point(513, 173);
+            chkAtivo.Name = "chkAtivo";
+            chkAtivo.Size = new Size(54, 19);
+            chkAtivo.TabIndex = 12;
+            chkAtivo.Text = "Ativo";
+            chkAtivo.UseVisualStyleBackColor = true;
+            // 
+            // txtConfSenha
+            // 
+            txtConfSenha.Location = new Point(379, 171);
+            txtConfSenha.Name = "txtConfSenha";
+            txtConfSenha.Size = new Size(115, 23);
+            txtConfSenha.TabIndex = 13;
+            txtConfSenha.UseSystemPasswordChar = true;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(390, 151);
+            label6.Name = "label6";
+            label6.Size = new Size(92, 15);
+            label6.TabIndex = 14;
+            label6.Text = "Confirma Senha";
+            // 
+            // FrmUsuario
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label6);
+            Controls.Add(txtConfSenha);
+            Controls.Add(chkAtivo);
+            Controls.Add(cmbNivel);
+            Controls.Add(txtSenha);
+            Controls.Add(txtEmail);
+            Controls.Add(txtNome);
+            Controls.Add(txtId);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnInserir);
+            Controls.Add(dgvUsuarios);
+            Name = "FrmUsuario";
+            Text = "FrmUsuario";
+            Load += FrmUsuario_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvUsuarios).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dgvUsuarios;
+        private DataGridViewTextBoxColumn clnid;
+        private DataGridViewTextBoxColumn clnNome;
+        private DataGridViewTextBoxColumn clmEmail;
+        private DataGridViewTextBoxColumn clnNivel;
+        private DataGridViewCheckBoxColumn clnAtivo;
+        private Button btnInserir;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox txtId;
+        private TextBox txtNome;
+        private TextBox txtEmail;
+        private TextBox txtSenha;
+        private ComboBox cmbNivel;
+        private CheckBox chkAtivo;
+        private TextBox txtConfSenha;
+        private Label label6;
+    }
+}

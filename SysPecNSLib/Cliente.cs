a8a285a599eb526071415f066a72a583184850f8
Mateus Teixeira <mateuscas78@gmail.com>
2024-09-10 15:33:55 -0300

    Criação da classe cliente

--- SysPecNSLib/Cliente.cs
+++ SysPecNSLib/Cliente.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data;
+using System.Reflection.Metadata.Ecma335;
 
 namespace SysPecNSLib
 {
@@ -17,6 +18,12 @@ namespace SysPecNSLib
         public string? Telefone { get; set; }
         public DateTime Data_nasc { get; set; }
         public DateTime Data_cad { get; set; }
+
+
+        public Cliente()
+        {
+          
+        }
 
         public Cliente(string? nome, string? email, string? telefone, string? cpf, DateTime data_nasc, DateTime data_cad)
         {
@@ -64,7 +71,7 @@ namespace SysPecNSLib
                 Id = dr.GetInt32(0);
             }
         }
-        /*public static Cliente ObterPorId(int id)
+        public static Cliente ObterPorId(int id)
         {
             Cliente cliente = new();
             var cmd = Banco.Abrir();
@@ -79,13 +86,62 @@ namespace SysPecNSLib
                     dr.GetString(2),
                     dr.GetString(3),
                     dr.GetBoolean(4),
-
-                    )
+                    dr.GetString(5),
+                    dr.GetDateTime(6),
+                    dr.GetDateTime(7)
+                    );
              
             }
-
-          */  
+            return cliente;
+          
         }
+        public static List<Cliente> ObterLista(string? nome = "")
+        {
+            List<Cliente> lista = new();
+            var cmd = Banco.Abrir();
+            cmd.CommandType = System.Data.CommandType.Text;
+            if (nome == "")
+            {
+                cmd.CommandText = "select * from clientes order by nome";
+            }
+            else
+            {
+                cmd.CommandText = $"select * from clientes where nome " +
+                    $"like '%{nome}%' order by nome";
+            }
+
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                lista.Add(
+                    new(
+                    dr.GetInt32(0),
+                    dr.GetString(1),
+                    dr.GetString(2),
+                    dr.GetString(3),
+                    dr.GetBoolean(4),
+                    dr.GetString(5),
+                    dr.GetDateTime(6),
+                    dr.GetDateTime(7)
+                    )
+                    );
+            }
+
+            return lista;
+        }
+        public void Atualizar() 
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_cliente_update";
+            cmd.Parameters.AddWithValue("spnome", Nome);
+            cmd.Parameters.AddWithValue("sptelefone", Telefone);
+            cmd.Parameters.AddWithValue("spdatanasc", Data_nasc);
+            cmd.ExecuteNonQuery();
+            cmd.Connection.Close();
+        } 
+
+        
         
     }
 }
